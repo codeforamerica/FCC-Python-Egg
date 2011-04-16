3ca029c6f3f9fce6bbf1d6462240e7f0e24cc490
johnfn <johnfn@gmail.com>
2011-04-16 16:04:49 -0700

    Broke into 2 files

--- broadband_api.py
+++ broadband_api.py
@@ -1,25 +1,7 @@
-import urllib
-import json
-
-# Inheritable class to perform requests to generic APIs.
-class BaseAPIRequest:
-  def __init__(self, url):
-    self.url = url
-
-  #Inherit this method to describe how to format URLs.
-  def format_url(self):
-    pass
-
-  # Requests the API and returns the JSON object.
-  def request(self, **args):
-    self.format_url(**args)
-    object = json.loads("".join([l for l in urllib.urlopen(self.url)]))
-    return object
-
-
-
+from generic_api import *
 
 # Simple Python wrapper around the Broadband API provided by the FCC.
+
 class BroadbandApi(BaseAPIRequest):
   def __init__(self):
     BaseAPIRequest.__init__(self, "http://data.fcc.gov/api/speedtest/find?latitude=%s&longitude=%s&format=json")
---
+++ generic_api.py
@@ -0,0 +1,17 @@
+import urllib
+import json
+
+# Inheritable class to perform requests to generic APIs.
+class BaseAPIRequest:
+  def __init__(self, url):
+    self.url = url
+
+  #Inherit this method to describe how to format URLs.
+  def format_url(self):
+    pass
+
+  # Requests the API and returns the JSON object.
+  def request(self, **args):
+    self.format_url(**args)
+    object = json.loads("".join([l for l in urllib.urlopen(self.url)]))
+    return object
