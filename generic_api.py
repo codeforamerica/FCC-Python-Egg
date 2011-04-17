e36c3054e21cfe80ac2d29533231275f954bf0f4
johnfn <johnfn@gmail.com>
2011-04-16 18:55:10 -0700

    Closures and generics and setattrs, oh my\!

--- generic_api.py
+++ generic_api.py
@@ -18,8 +18,7 @@ class BaseAPIRequest:
 
   #Inherit this method to describe how to format URLs.
   def format_url(self, **args):
-    if args is None:
-      raise NoArgumentsException
+    if args is None: raise NoArgumentsException
 
     args["format"] = "json" #Ask for JSON formatted response
     append = ""
@@ -36,14 +35,47 @@ class BaseAPIRequest:
     t = urllib.urlopen(self.formatted_url).read().strip()
     if t.startswith("callback("):
       t=t[t.index("(")+1:-1]
-    
+
+    return json.loads(t)
+
+    """
     try:
-      return json.loads(t)
+      
     except:
       raise BadJSONException
       return None
+    """
 
     #object = json.loads("".join([l for l in urllib.urlopen(self.formatted_url)]))
+
+
+class GenericAPI:
+  def __init__(self, apis):
+
+    self.api_objects   = []
+    self.api_functions = []
+    self.functions     = []
+
+    #Who writes normal functions when you can use CLOSURES?!?
+
+    number = 0
+
+    for api in apis:
+      self.api_objects.append(BaseAPIRequest(api[1]))
+      self.bind_closure(number)
+      setattr(self, api[0], self.api_functions[number])
+      number += 1
+
+  def bind_closure(self, number):
+      def generic_api_call(**kwargs):
+        self.api_objects[number].format_url(**kwargs)
+        return self.api_objects[number].request(**kwargs)
+      
+      self.api_functions.append(generic_api_call)
+
+
+
+
 
 
 # Wraps any API that just uses Latitude and Longitude.
--- license_view_api.py
+++ license_view_api.py
@@ -2,27 +2,12 @@ from generic_api import *
 
 # Simple Python wrapper around the License View API provided by the FCC.
 
-# The main License View API class. Exposes several API calls as
-# functions.
 
-class LicenseViewAPI:
-  def __init__(self):
-    self.get_license_api = BaseAPIRequest("http://data.fcc.gov/api/license-view/basicSearch/getLicenses")
-    self.get_license_api = BaseAPIRequest("http://data.fcc.gov/api/license-view/basicSearch/getLicenses")
-
-
-
-
-  # Example usage:
-  #
-  # get_licenses(search_value = "Verizon Wireless")
-
-  def get_licenses(self, **kwargs):
-    self.get_license_api.format_url(**kwargs)
-    return self.get_license_api.request(**kwargs)
-
+apis = [ ("get_licenses", "http://data.fcc.gov/api/license-view/basicSearch/getLicenses" )
+       , ("get_common_names", "http://data.fcc.gov/api/license-view/licenses/getCommonNames")
+       ]
 
 # Sample use of BlockConversionAPI
 if __name__ == "__main__":
-  bc = LicenseViewAPI()
+  bc = GenericAPI(apis)
   print bc.get_licenses(searchValue = "Verizon Wireless")
